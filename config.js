a5b8517e6db33bad5485491ee51145403e3b8920
Vitomir Budimir <budimirvitomir@gmail.com>
2020-04-01 11:31:26 +0200

    Setup AWS Amplify

---
+++ config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "serverless-react-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://i88yjfhnsc.execute-api.eu-central-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_bCV9nOVKJ",
+    APP_CLIENT_ID: "7feei7o62l0kn39ejgucqak798",
+    IDENTITY_POOL_ID: "eu-central-1:3abf495d-7a32-4b8c-81f8-71298b562bb8"
+  }
+};
