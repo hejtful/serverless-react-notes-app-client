11374f2b99608ed1c8e19fb7b9dda88ba8ff652a
Vitomir Budimir <budimirvitomir@gmail.com>
2020-04-01 15:38:13 +0200

    Add Signup feature

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import NotFound from "./containers/NotFound";
 
 export default function Routes({ appProps }) {
@@ -11,6 +12,12 @@ export default function Routes({ appProps }) {
     <Switch>
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
       <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute
+        path="/signup"
+        exact
+        component={Signup}
+        appProps={appProps}
+      />
       <Route component={NotFound} />
     </Switch>
   );
