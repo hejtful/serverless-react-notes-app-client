74b087ecef41d54da36a2f4512f91e56b0e1f28b
Vitomir Budimir <budimirvitomir@gmail.com>
2020-04-02 11:49:52 +0200

    Add and apply authenticated and unauthenticated route common components

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import AppliedRoute from "./components/AppliedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
@@ -14,26 +16,31 @@ export default function Routes({ appProps }) {
   return (
     <Switch>
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
-      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
-      <AppliedRoute
+      <UnauthenticatedRoute
+        path="/login"
+        exact
+        component={Login}
+        appProps={appProps}
+      />
+      <UnauthenticatedRoute
         path="/signup"
         exact
         component={Signup}
         appProps={appProps}
       />
-      <AppliedRoute
+      <AuthenticatedRoute
         path="/settings"
         exact
         component={Settings}
         appProps={appProps}
       />
-      <AppliedRoute
+      <AuthenticatedRoute
         path="/notes/new"
         exact
         component={NewNote}
         appProps={appProps}
       />
-      <AppliedRoute
+      <AuthenticatedRoute
         path="/notes/:id"
         exact
         component={Notes}
